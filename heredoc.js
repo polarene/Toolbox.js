64f31b55dd553f4e4aecf13d56227f204123bfae
Matteo <polaretto@gmail.com>
2011-06-19 19:17:31 +0200

    first file

---
+++ heredoc.js
@@ -0,0 +1,18 @@
+/**
+ * 	here-doc function for using multiline strings in javascript
+ *	( http://stackoverflow.com/questions/805107/multiline-strings-in-javascript/5571069#5571069 )
+ *
+ *	usage:
+ *	var tennysonQuote = hereDoc(function() {/*!
+ *	  Theirs not to make reply,
+ *	  Theirs not to reason why,
+ *	  Theirs but to do and die
+ *	*/}); 
+ */
+
+function hereDoc(f) {
+  return f.toString().
+      replace(/^[^\/]+\/\*!?/, '').
+      replace(/\*\/[^\/]+$/, '');
+}
+
